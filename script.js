a16013cac736ce7cccc09a9bb57c76afb372f2ae
shivanandreddy <31992316+shivanandreddy@users.noreply.github.com>
2023-11-04 18:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let bars=document.querySelector("#bars")
+let sideNav=document.querySelector("#sideNav")
+
+let hideSideNav=()=>{
+    if(sideNav.style.display==="block"){
+        sideNav.style.display="none"
+    }
+    else{
+        sideNav.style.display="block"
+    }
+}
+
